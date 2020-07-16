79f0a36fe63827b9a256af79a059a6bfc4063824
Herodotus <termlimit@gmail.com>
2020-07-16 00:04:48 -0400

    Updated autoloader src directory

--- src/ORM/MySqliDB.php
+++ src/ORM/MySqliDB.php
@@ -7,7 +7,8 @@ class MySqliDB
 	{
 		// Connecting to and selecting a MySQL database named sakila
 		// Hostname: 127.0.0.1, username: your_user, password: your_pass, db: sakila
-		return class_exists("mysqli");
+		echo class_exists("mysqli");
+		exit;
 		$mysqli = new mysqli($server, $database_username, $database_password, $database_name);
 
 		// Oh no! A connect_errno exists so the connection attempt failed!
