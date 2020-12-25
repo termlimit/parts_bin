435efbc37ac8f21e4079089334e092a2152fcbef
Parts Bin <parts@partsbin.loadedfinance.com>
2020-12-25 17:50:24 +0000

    Moving to CakePHP framework

--- config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Configure paths required to find general filepath
- * constants
- */
-require __DIR__ . '/paths.php';
-
-// Use composer to load the autoloader.
-require ROOT . DS . 'vendor' . DS . 'autoload.php';
--- src/ORM/DataSource.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace ORM;
-
-class DataSource
-{
-	
-}
